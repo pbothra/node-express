20f90fe2953fd6675e03d46a3b9c80828016566a
Pawan Bothra <pawan_bothra@persistent.co.in>
2019-11-28 15:37:56 +0530

    Assignment 1

---
+++ MyCode/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const hostname = 'localhost';
+const port = 3000;
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is Express Server</h1></body></html>');
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server is Running at http://${hostname}:${port}`);
+});
--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const http = require('http');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-const hostname = 'localhost';
-const port = 3000;
-const dishRouter = require('./routes/dishRouter');
+const HOSTNAME = 'localhost';
+const PORT = 3000;
 
 const app = express();
+const morgan = require('morgan');
+
+//This needs to be BEFORE app.use((req,res,next)=>{...});
 app.use(morgan('dev'));
-app.use(bodyParser.json());
+
+
+//Routers:
+const dishRouter = require('./routes/dishRouter');
 app.use('/dishes', dishRouter);
 
+const promotionRouter = require('./routes/promoRouter');
+app.use('/promotions', promotionRouter);
 
+const leaderRouter = require('./routes/leaderRouter');
+app.use('/leaders', leaderRouter);
+
+
+//This needs to be BEFORE app.use((req,res,next)=>{...});
 app.use(express.static(__dirname + '/public'));
+
+//It seems this needs to be the LAST app.use() statement.
 app.use((req, res, next) => {
+    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is Express Server</h1></body></html>');
+    res.end('<html><body><h1>This is an Express server!</h1></body></html>');
 });
 
 const server = http.createServer(app);
 
-server.listen(port, hostname, () => {
-    console.log(`Server is Running at http://${hostname}:${port}`);
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Express server running at http://${HOSTNAME}:${PORT}/`);
 });
